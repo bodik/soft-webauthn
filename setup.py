ac07ea1a23543862493b4fce82a7a6058b6f7a94
Radoslav Bodo <bodik@cesnet.cz>
2019-07-03 07:33:03 +0000

    general: add install_requires

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     url='https://github.com/bodik/soft_webauthn',
     py_modules=['soft_webauthn'],
+    install_requires=['fido2', 'cryptography'],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
